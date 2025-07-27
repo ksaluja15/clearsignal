6cb745c76cbd432f814d983025363aec9d28eb4a
Kunal Saluja <kunal.saluja@flocksafety.com>
2025-07-28 02:17:08 +0530

    improved bg

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -7,8 +7,9 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ children, className = '' }) => (
     <div
-        className={ `outline-solid outline-1 shadow-blue-400 rounded-lg shadow-md p-1 transition-all duration-300 
-               hover:bg-blue-400/50 hover:scale-105 ${className}`}
+        className={ `rounded-lg shadow-md p-4 transition-all duration-300 
+               hover:bg-blue-400/50 hover:scale-105 ${className}` }
+        style={{ backgroundColor: '#151819' }}
     >
         {children}
     </div>
